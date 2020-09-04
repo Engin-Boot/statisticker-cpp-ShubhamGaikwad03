f884e224fe633d05e38923ca3a33c9676a09eda9
Shubham <Shubham.gaikwad88.15@gmail.com>
2020-09-04 22:22:15 +0530

    Stats cpp value updated

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,26 @@
 #include "stats.h"
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+using namespace std;
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& value) {
     //Implement statistics here
+    Stats st;
+    if (value.empty())   // Error condition
+    {
+        st.average = NAN;
+        st.min = NAN;
+        st.max = NAN; 
+        return st;
+    }
+    float sum=0;
+    st.min = *min_element(value.begin(),value.end());
+    st.max = *max_element(value.begin(), value.end());
+    for (int i = 0; i < value.size(); i++)
+    {
+        sum = sum + value[i];
+    }
+    st.average = sum/value.size();
+    return st;
 }
