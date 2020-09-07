c10edf9fcd63c01d4a1578f7a3f9243eb791b844
Shubham <Shubham.gaikwad88.15@gmail.com>
2020-09-07 13:06:43 +0530

    Update Stat Test

--- stats-test.cpp
+++ stats-test.cpp
@@ -13,16 +13,6 @@ TEST_CASE("reports average, minimum and maximum") {
     REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
 }
 
-
-TEST_CASE("reports average, minimum and maximum for array containing atleast one NaN input") 
-{
-    auto computedStats = Statistics::ComputeStatistics({2.1, 7.9,2.5,NAN, 4.5,NAN});
-    float epsilon = 0.001;
-    REQUIRE(std::abs(computedStats.average - 4.25) < epsilon);
-    REQUIRE(std::abs(computedStats.max - 7.9) < epsilon);
-    REQUIRE(std::abs(computedStats.min - 2.1) < epsilon);
-}
-
 TEST_CASE("average is NaN for empty array") {
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h 
